de4530fb21d0e0e0475964e0a823cdcf9a610e8a
dueonkim89 <dueonkim89@yahoo.com>
2021-11-05 00:37:29 -0700

    work on POST route for microservice, update venv files

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, flash, redirect
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "A message from CS361"
+    return render_template('index.html')
+
+
+@app.route("/selection", methods=["GET", "POST"])
+def selection():
+    if request.method == "POST":
+        number = request.form.get("select_number_of_countries")
+        print("******************** number is: ", number)
+        # make request to microservice, requires a parameter of numbers at the end
+        # https://stackoverflow.com/questions/15463004/how-can-i-send-a-get-request-from-my-flask-app-to-another-site
+        micro_service_url = 'https://malliuxservice.herokuapp.com/countries/'
+        return(number)
+    else:
+        return render_template('selection.html')
