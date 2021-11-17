2f7ff90fcff8570e823866fd0d6ca4774339fca6
unknown <dueonkim89@Yahoo.com>
2021-11-16 18:52:24 -0800

    send POST request from client to server on click event

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ import requests
 from utils import decode_to_string, get_countries_from_string
 
 app = Flask(__name__)
-cache = None
+
+# Global variables to cache user choice
+global cache
+cache = {}
+global chosen_country
+global cached_recipes
+cached_recipes = []
+
 spoonacular_api_key = "81bd5951a8ee44bc9d54fbb1858704b0"
+countries_in_spoonacular = {'French', 'Thai', 'Italian', 'Indian', 'Spanish', 'Greek', 'Mexican'}
+countries_in_all_recipes = {'Turkey', 'Argentina', 'Portgual'}
 
 @app.route("/")
 def index():
@@ -21,8 +30,26 @@ def selection():
         r = requests.get(micro_service_url)
         data = decode_to_string(r.content)
         arr_of_countries = get_countries_from_string(data)
-        cache = arr_of_countries
         # render html based on number of countries in arr_of_countries
         return render_template('country_list.html', countries=arr_of_countries)
     else:
         return render_template('selection.html')
+
+
+@app.route("/recipes_for_country", methods=["POST"])
+def find_recipe():
+    # parse JSON
+    data = request.json
+    country = data['country']
+    global chosen_country
+    chosen_country = country
+    recipe_list = []
+
+    # make request to spoonacular API and get all recipes
+    if country in countries_in_spoonacular:
+        print(data['country'])
+        return request.data
+    # else, scrap data from website all_recipes and get all recipes
+
+
+    # gather recipe data and redirect to country_recipes.html
--- static/script.js
+++ static/script.js
@@ -1,9 +1,35 @@
-console.log("hello world")
-
 const countrySearchWord = {'France': 'French', 'Thailand': 'Thai', 'Italy': 'Italian', 
 'India': 'Indian', 'Spain': 'Spanish', "Greece": 'Greek', 'Mexico': 'Mexican', 
 'Turkey': 'Turkey', 'Argentina': 'Argentina', 'Portugal': 'Portugal'};
 
-// add event listener for list-group
-// send a fetch request to server POST with value of country
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+// DOM elements to set event listeners
+const countryList = document.querySelectorAll(".country.list-group-item.list-group-item-action");
+
+// add event listener for each country in country list-group
+for (let i = 0; i<countryList.length; i++) {
+    countryList[i].addEventListener("click", function(){
+        makeRequestForCountryRecipe(countryList[i].innerText)
+    });
+}
+
+function makeRequestForCountryRecipe(country) {
+    // send a fetch request to server POST with key value in countrySearchWord
+    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    const data = {"country": countrySearchWord[country]};
+    fetch('/recipes_for_country', {
+    method: 'POST', // or 'PUT'
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    }).then(response => response.json())
+    .then(result => {
+      console.log('Success:', result);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+   
+}
+
+
