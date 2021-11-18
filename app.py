af37353248491697d8a01d546efc34c0a61c065b
unknown <dueonkim89@Yahoo.com>
2021-11-17 17:42:31 -0800

    Render recipes from API request. Cache API results to prevent duplicate requests

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ from utils import decode_to_string, get_countries_from_string
 app = Flask(__name__)
 
 # Global variables to cache user choice
-cache = {}
+cached_countries = None
 chosen_country = None
 cached_recipe_list_per_country = {}
 cached_recipes = {}
 
+# api key
 spoonacular_api_key = "81bd5951a8ee44bc9d54fbb1858704b0"
-countries_in_spoonacular = {'French', 'Thai', 'Italian', 'Indian', 'Spanish', 'Greek', 'Mexican'}
-countries_in_all_recipes = {'Turkey', 'Argentina', 'Portgual'}
+
+country_search_word = {'France': 'French', 'Thailand': 'Thai', 'Italy': 'Italian', 
+'India': 'Indian', 'Spain': 'Spanish', "Greece": 'Greek', 'Mexico': 'Mexican', 
+'Turkey': 'Middle+Eastern', 'Argentina': 'Latin+American', 'Portugal': 'Mediterranean'}
 
 @app.route("/")
 def index():
@@ -28,50 +31,46 @@ def selection():
         micro_service_url += number
         r = requests.get(micro_service_url)
         data = decode_to_string(r.content)
-        arr_of_countries = get_countries_from_string(data)
+        global cached_countries
+        cached_countries = get_countries_from_string(data)
         # render html based on number of countries in arr_of_countries
-        return render_template('country_list.html', countries=arr_of_countries)
+        return render_template('country_list.html', countries=cached_countries)
     else:
         return render_template('selection.html')
+
+
+@app.route("/country_list")
+def country_list():
+    return render_template('country_list.html', countries=cached_countries)
 
 
 @app.route("/recipes_for_country", methods=["GET", "POST"])
 def find_recipe():
     # POST route
     if request.method == "POST":
-        # parse JSON
-        data = request.json
-        country = data['country']
+        country = country_search_word[request.form.get("countryRadios")]
         global chosen_country
         chosen_country = country
     
-        # make request to spoonacular API and get all recipes
-        if country in countries_in_spoonacular:
-            # if in cache, pass info from cache into HTML template
-            if country in cached_recipe_list_per_country:
-                print("LINE 52")
-                print(cached_recipe_list_per_country[country]['results'])
-                return render_template('country_recipes.html', country=country)
-            else:
-                # make API call to spoonacular    
-                base_url = "https://api.spoonacular.com/recipes/complexSearch?apiKey="
-                base_url += spoonacular_api_key
-                base_url += '&cuisine='
-                base_url += country
-                response = requests.get(base_url)
+        # if in cache, pass info from cache into HTML template
+        if country in cached_recipe_list_per_country:
+            return render_template('country_recipes.html', country=request.form.get("countryRadios"), recipes=cached_recipe_list_per_country[country]['results'])
+        else:
+             # make request to spoonacular API and get all recipes
+            base_url = "https://api.spoonacular.com/recipes/complexSearch?apiKey="
+            base_url += spoonacular_api_key
+            base_url += '&cuisine='
+            base_url += country
+            response = requests.get(base_url)
 
-                # store data into global cache, to avoid repetitive API calls
-                cached_recipe_list_per_country[country] = response.json()
+            # store data into global cache, to avoid repetitive API calls
+            cached_recipe_list_per_country[country] = response.json()
 
-                # gather recipe data and redirect to country_recipes.html
-                print("LINE 67")
-                print(cached_recipe_list_per_country[country]['results'])
-                return render_template('country_recipes.html', country=country)
+            # gather recipe data and redirect to country_recipes.html
+            return render_template('country_recipes.html', country=request.form.get("countryRadios"), recipes=cached_recipe_list_per_country[country]['results'])
 
-        # else, scrap data from website all_recipes and get all recipes
-        
     # GET request, send recipe list from cached country
-
+        
 
 # route to get specific recipe instruction
         
