b4b8febacaff30e3f658e6f0da239a6e748d0ea0
unknown <dueonkim89@Yahoo.com>
2021-11-17 17:40:58 -0800

    use form to render HTML template from server. No time for SPA

--- utils.py
+++ utils.py
@@ -6,13 +6,15 @@ def get_countries_from_string(string):
     '''extract the countries from the string'''
     arr_of_countries = []
     start = None
+    counter = 1
     # loop and slice countries from string
     for idx in range(len(string)):
         # " can be start or end of string
         if string[idx] == '"':
             # next char is a comma or closing bracket, indicates end of string
             if string[idx + 1] == ',' or string[idx + 1] == ']':
-                arr_of_countries.append(string[start:idx])
+                arr_of_countries.append({'name': string[start:idx], 'id': "country" + str(counter)})
+                counter += 1
             # else its the start of the string
             else:
                 start = idx + 1
